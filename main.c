b9b4517308029a66cf320f55006674e6f204278a
GriFFen75 <savadouxbaptiste25@gmail.com>
2022-05-21 14:44:12 -0400

    echo fonctionne, plante au niveau des malloc et realloc

--- main.c
+++ main.c
@@ -6,12 +6,13 @@
 //#include "mylib/mystring.c" mettre celui la si on veut faire avec le terminal
 
 
+int NbArguments;
 
 char * shell_read_line(){ //fonctione pour lire les ligne rentré
     char *commande ;
     size_t len = mystrlen(commande);
     size_t caractere;
-    commande = (char *) malloc(len * sizeof(char));
+    commande = (char *) malloc( len * sizeof(char));
 
     caractere = getline(&commande,&len,stdin);
 
@@ -23,20 +24,23 @@ char * shell_read_line(){ //fonctione pour lire les ligne rentré
 
 char** shell_split_line(char * line){ //separe la ligne recupere
     char *commande;
-    char **splitCommande = malloc (sizeof(splitCommande));
+    char **splitCommande = malloc (sizeof(splitCommande)); //ici je pense
     //size_t tailleCommande = mystrlen(line)+1;
     int i = 0;
     char *strToken = mystrtok(line);
     commande=strToken;
     while (strToken != NULL){
         //mystrcat(splitCommande,strToken);
+//        splitCommande = realloc(&*splitCommande,sizeof (splitCommande)*i);
         splitCommande[i] = strToken;
         //printf("Token = %s\n",strToken);
         strToken=mystrtok(NULL);
-        printf("Le mot %d est : %s\n",i,splitCommande[i]);
+        //printf("Le mot %d est : %s\n",i,splitCommande[i]);
         i++;
+        //printf ("il y a %d token\n",i);
     }
-    printf("la commande apres strtok est : %s\n",commande);
+    NbArguments=i;
+    //printf("la commande apres strtok est : %s\n",commande);
     return (splitCommande);
 }
 
@@ -57,7 +61,10 @@ int shell_execute(char** chaineSplit){ // faire la fonction stpcmp()
         printf("\nle ls est bon\n");
     }
     else if (mystrcmp(commande,"echo")==0){//juste faire un printf de ce qu'il y a en parametre
-        printf("\nle echo est bon\n");
+        printf("\n");
+        for (int i = 1 ; i < NbArguments ; i++){
+            printf("%s ",chaineSplit[i]);
+        }
     }
     else if (mystrcmp(commande,"md")==0){ //mkdir
         printf("\nle md/mkdir est bon\n");
@@ -76,12 +83,16 @@ void shell_loop(void){
     do {
         printf("\nEsiea_shell_> ");
         line = shell_read_line();
+//        line = realloc(&line,sizeof(shell_read_line()));
+        chaineSplit = realloc(&**chaineSplit,sizeof(shell_split_line(line))* NbArguments);
         chaineSplit = shell_split_line(line); //separation commande et option
         printf("dans shell_loop chaineSplit[0] : %s\n",chaineSplit[0]);
         printf("printf dans shell_loop : %s\n",line);
         status = shell_execute(chaineSplit);
-        free(line);
-        free(chaineSplit);
+//        line = realloc(&line,sizeof (&line  NbArguments));
+//        chaineSplit = realloc(&chaineSplit,sizeof (shell_split_line(line)));
+//        free(line);
+//        free(chaineSplit);
     } while (status);
 }
 
