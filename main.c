2962660c33d4b268b2e1f9048342dede962988ed
GriFFen75 <savadouxbaptiste25@gmail.com>
2022-05-22 10:14:29 -0400

    avancement dans les commandes : ls fait : echo/pwd/exit/cd

--- main.c
+++ main.c
@@ -3,8 +3,10 @@
 #include <unistd.h>
 
 #include "mylib/mystring.h"
-//#include "mylib/mystring.c" mettre celui la si on veut faire avec le terminal
+//#include "mylib/mystring.c" //mettre celui la si on veut faire avec le terminal
 
+
+//la foncione pipe et fork peuvent etre utile .
 
 int NbArguments;
 int Nbcaractere;
@@ -17,7 +19,7 @@ char * shell_read_line(){ //fonctione pour lire les ligne rentré
 
     caractere = getline(&commande,&len,stdin);
 
-    printf("Len : %zu\nprint dans shell_read_line : %s\nLe nombre de caractère : %ld \n",len,commande,caractere);
+//    printf("Len : %zu\nprint dans shell_read_line : %s\nLe nombre de caractère : %ld \n",len,commande,caractere);
     commande[caractere-1]='\0'; //commande[caractere],
     Nbcaractere=caractere;
     return commande;
@@ -43,30 +45,67 @@ char** shell_split_line(char * line){ //separe la ligne recupere
 
 int shell_execute(char** chaineSplit){ // faire la fonction stpcmp()
     char *commande = chaineSplit[0];
-    printf("La chaineSplit[0] dans shell_execute : |%s|\n",chaineSplit[0]);
-    printf("La commande dans shell_execute : |%s|",commande);
+//    printf("La chaineSplit[0] dans shell_execute : |%s|\n",chaineSplit[0]);
+//    printf("La commande dans shell_execute : |%s|",commande);
 
     //commencement des commandes
     if (mystrcmp(commande,"cd")==0){
-        printf("\nle cd est bon\n");
+        char S[255];
+        const char * path = getenv( "PATH" );
+        printf ("PATH environement variable == %s\n",path);
+        const char * home = getenv( "HOME" );
+        if (chaineSplit[1]==NULL){
+            chdir(home);
+        }
+        else{
+            chdir(chaineSplit[1]);
+        }
+        getcwd(S,255);
+        printf("Repertoire courant %s\n",S);
     }
     else if (mystrcmp(commande,"ls")==0){
-        printf("\nle ls est bon\n");
+        FILE * sortie = popen("ls","r");
+        if (sortie == NULL){
+            printf ("\nLa sortie est égale a nul\n");
+        }
+        else{
+//            fprintf(sortie,"%p\n");
+            char *result;
+            fgets(result, 512, sortie);
+            printf("%s\n", result);
+        }
+        pclose(sortie);
     }
     else if (mystrcmp(commande,"echo")==0){//juste faire un printf de ce qu'il y a en parametre
         printf("\n");
         for (int i = 1 ; i < NbArguments ; i++){
-            printf("%s ",chaineSplit[i]);
+            //printf("%s ",chaineSplit[i]);
+            if (chaineSplit[i]==NULL){
+                printf("Vous n'avez rien écrit en paramètre");
+            }
+            else{
+                write(1,chaineSplit[i],mystrlen(chaineSplit[i]));
+                write(1," ",1);
+            }
         }
     }
     else if (mystrcmp(commande,"md")==0){ //mkdir
         printf("\nle md/mkdir est bon\n");
+
     }
+    else if (mystrcmp(commande,"history")==0){
+        printf("\nle history est bon\n");
+    }
+    else if (mystrcmp(commande,"pwd")==0){
+        const char * pathRepertoireCourant = getenv( "PWD" );
+        printf("Le path du repertoire courant est : %s\n",pathRepertoireCourant);
+    }
+
     else if (mystrcmp(commande,"help")==0){
         printf("\nle help est bon\n");
     }
     else if (mystrcmp(commande,"exit")==0){
-        exit(0);
+        return EXIT_SUCCESS;
     }
     else{
         printf ("\nCe que vous avez ecris n'est pas une commande\nConsultez la liste des commandes possible avec help");
@@ -87,8 +126,8 @@ void shell_loop(void){
 //        line = realloc(&line,sizeof(shell_read_line()));
 //
         chaineSplit = shell_split_line(line); //separation commande et option
-        printf("dans shell_loop chaineSplit[0] : %s\n",chaineSplit[0]);
-        printf("printf dans shell_loop : %s\n",line);
+//        printf("dans shell_loop chaineSplit[0] : %s\n",chaineSplit[0]);
+//        printf("printf dans shell_loop : %s\n",line);
         status = shell_execute(chaineSplit);
     } while (status);
 }
